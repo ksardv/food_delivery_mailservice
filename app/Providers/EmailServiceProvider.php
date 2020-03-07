6621aa2839b7f2647abdc6f6b3de33e9982815d5
ksardv <kolyo.sardamov@gmail.com>
2020-03-07 13:54:03 +0000

    initial commit

---
+++ app/Providers/EmailServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Lumen\Providers\EmailServiceProvider as ServiceProvider;
+
+class EmailServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton(MailjetConnector::class, function ($app) {
+            $config = $app->make('config')->get('services.mailjet', []);
+            return new MailjetConnector($config);
+        });
+
+        $this->app->singleton(SendgridConnector::class, function ($app) {
+            $config = $app->make('config')->get('services.sendgrid', []);
+            return new SendgridConnector($config);
+        });
+    }
+}
